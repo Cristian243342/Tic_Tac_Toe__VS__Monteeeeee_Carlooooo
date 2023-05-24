e54aa3e32bce5f82fd168575f9d63b120d1094e7
EugenM03 <geanyzz91@gmail.com>
2023-05-24 11:14:13 +0300

    TODO: add comments

--- struct.h
+++ struct.h
@@ -15,7 +15,7 @@ typedef struct list_t {
 typedef struct mc_node_t {
 	int32_t wins, sims;
 	double value;
-	int8_t turn, child_nr, max_children; // -1 for 0 and 1 for X; move_nr = nr of children
+	int8_t turn, child_nr, max_children; // -1 for 0 and 1 for X; move_nr = nr of children (?)
 	list_t *child_list;
 	int8_t **image;
 	struct mc_node_t *parent;
